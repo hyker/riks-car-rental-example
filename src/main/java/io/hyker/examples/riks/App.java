ce831a85c64196d09a7fb87ddf25a5db824ec939
Joakim Brorsson <joakim@hyker.io>
2017-03-29 14:02:30 +0200

    initial

---
+++ src/main/java/io/hyker/examples/riks/App.java
@@ -0,0 +1,13 @@
+package io.hyker.examples.riks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

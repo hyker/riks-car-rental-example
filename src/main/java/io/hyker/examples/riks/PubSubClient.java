72237f844e6155d1171cc21a36bc8c4d50744ba4
Joakim Brorsson <b.joakim@gmail.com>
2017-05-16 11:36:22 +0200

    latest riks

--- src/main/java/io/hyker/examples/riks/PubSubClient.java
+++ src/main/java/io/hyker/examples/riks/PubSubClient.java
@@ -1,13 +1,5 @@
 package io.hyker.examples.riks;
 
-import io.apptimate.cryptobox.CryptoBox;
-import io.apptimate.cryptobox.CryptoBoxCredentialsException;
-import io.apptimate.riks.box.RiksKit;
-import io.apptimate.riks.box.RiksWhitelist;
-import io.apptimate.riks.keys.SymKeyExpiredException;
-import io.apptimate.riks.message.Message;
-import io.apptimate.riks.message.RiksMessage;
-import io.apptimate.security.publickeylookup.PublicKeyLookupException;
 import org.bouncycastle.crypto.CryptoException;
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
